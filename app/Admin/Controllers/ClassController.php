651186f1e383807511a255d64bc8081dcdfbaaa0
JoseChan <524233828@qq.com>
2020-01-22 16:39:03 +0800

    fix

--- app/Admin/Controllers/ClassController.php
+++ app/Admin/Controllers/ClassController.php
@@ -119,6 +119,8 @@ class ClassController extends Controller
             $form->image('pic',"课程图片")->move("classes/images");
             $form->datetime('start_time',"上课时间");
             $form->datetime('end_time',"下课时间");
+            $form->radio('is_buy', "是否购买")->options(['1' => '是', '0'=> '否'])->default('0');
+            $form->text('price', "价格");
             $form->text('start_age', "最小适龄(0为不限)")->rules("Integer");
             $form->text('end_age', "最大适龄（0为不限）")->rules("Integer");
 
