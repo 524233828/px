87edf4442dd2f3acdc4bdc79385cbc433213b3b1
JoseChan <524233828@qq.com>
2020-05-03 15:54:05 +0800

    fix

--- config/payment.php
+++ config/payment.php
@@ -7,7 +7,7 @@ return [
         'mch_id'     => env('WECHAT_PAY_MCH_ID'), // 微信支付商户号
         'mch_secret' => env('WECHAT_PAY_MCH_SECRET'), // 微信支付密钥
         "notify_url" => env("APP_URL")."/api/order/notify",
-        "cert" => env("WECHAT_PAY_MCH_CERT"),//支付证书
-        "ssl_key" => env("WECHAT_PAY_MCH_KEY"),//支付证书密钥
+        "cert" => file_get_contents(env("WECHAT_PAY_MCH_CERT")),//支付证书
+        "ssl_key" => file_get_contents(env("WECHAT_PAY_MCH_KEY")),//支付证书密钥
     ],
 ];
